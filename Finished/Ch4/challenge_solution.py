05c5760653fded1edd43774a72b095d30a5bf68d
Joe Marini <cjmarini68@gmail.com>
2024-05-06 18:59:23 -0700

    fix headers issue

--- Finished/Ch4/challenge_solution.py
+++ Finished/Ch4/challenge_solution.py
@@ -9,18 +9,18 @@ import pandas as pd
 # read the original data set
 df_sales = pd.read_excel("FinancialSample.xlsx")
 
-
+headers = ["Product", "Gross Sales", "Profits"]
 # calculate the summary data
-summary_data = [
-    ["Product","Gross Sales","Profits"]
-]
+summary_data = []
 for prod_name in df_sales["Product"].unique():
-    sales_total = df_sales.loc[df_sales['Product'] == prod_name, "Gross Sales"].sum()
-    profit_total = df_sales.loc[df_sales['Product'] == prod_name, "Profit"].sum()
+    sales_total = df_sales.loc[df_sales['Product']
+                               == prod_name, "Gross Sales"].sum()
+    profit_total = df_sales.loc[df_sales['Product']
+                                == prod_name, "Profit"].sum()
     summary_data.append([prod_name, sales_total, profit_total])
 
 # create a new DataFrame with the summary data
-df_summary = pd.DataFrame(summary_data)
+df_summary = pd.DataFrame(summary_data, columns=headers)
 
 # write the data to the new sheet
 with pd.ExcelWriter("FinancialSample.xlsx", engine="openpyxl", mode='a') as xlw:
